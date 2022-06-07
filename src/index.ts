b4f12e711f3cb1ee346faf9b51d6f4aa30c2039d
dpisati <43629737+dpisati@users.noreply.github.com>
2022-06-07 12:43:13 +1200

    final

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-import express from "express";
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger.json';
 
-import { usersRoutes } from "./routes/users.routes";
+import { usersRoutes } from './routes/users.routes';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.use("/users", usersRoutes);
+app.use('/users', usersRoutes);
 
 export { app };
